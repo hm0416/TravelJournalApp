74132859300e1904b939f84876aab0cec6dac0f2
hm0416 <mousou.3@buckeyemail.osu.edu>
2021-10-06 16:37:36 -0400

    got fragment working for country button

--- app/src/main/java/com/example/traveljornal/DashboardActivity.java
+++ app/src/main/java/com/example/traveljornal/DashboardActivity.java
@@ -9,15 +9,34 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DashboardActivity extends AppCompatActivity {
 
     Button mapBtn;
+    Button country;
+    TextView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        search = (TextView) findViewById(R.id.searchTxt);
+        country = (Button) findViewById(R.id.countryBtn) ;
+        country.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fr = new CountryFragment();
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.container, fr).addToBackStack("fragment_country").commit();
+                country.setVisibility(View.GONE);
+                search.setVisibility(View.GONE);
+
+            }
+        });
 
         mapBtn = (Button) findViewById(R.id.mapButton);
         mapBtn.setOnClickListener(new View.OnClickListener() {
