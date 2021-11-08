d85ce5446d642873f58d674f7a5f23015685d60c
hm0416 <hm0416@hifas-mbp-8.attlocal.net>
2021-11-07 22:00:15 -0500

    multiple markers works, but not saving

--- app/src/main/java/com/example/traveljornal/MapsActivity.java
+++ app/src/main/java/com/example/traveljornal/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.example.traveljornal;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +26,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
+    //https://stackoverflow.com/questions/44165099/saving-map-marker-location-onmapclick-using-shared-preferences/44166482
+    public static final String SHARED_PREF_NAME = "plot";
+    public static final String LONGTITUDE = "long";
+    public static final String LATITUDE = "lat";
+    public static final String PLOTTED = "plotted";
+    private boolean plotted = false;
+    public static final String LOGGEDIN_SHARED_PREF = "";
+    public int markerCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +46,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+//        prefs = this.getSharedPreferences("LatLng",MODE_PRIVATE);
+//
+//        if((prefs.contains("Lat")) && (prefs.contains("Lng"))) {
+//            String lat = prefs.getString("Lat", "");
+//            String lng = prefs.getString("Lng", "");
+//            LatLng l = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+//            mMap.addMarker(new MarkerOptions().position(l));
+//        }
     }
 
     /**
@@ -45,8 +69,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ArrayList<LatLng> locationArrayList = new ArrayList<>();
 
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -54,12 +79,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions.position(latLng);
                 markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                mMap.clear();
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                mMap.addMarker(markerOptions);
+                locationArrayList.add(latLng);
+
+//                mMap.clear();
+
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                markerCount++;
+
+                String lat = Double.toString(latLng.latitude);
+                String lon = Double.toString(latLng.longitude);
+
+                SharedPreferences sharedPreferences = MapsActivity.this.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(LOGGEDIN_SHARED_PREF, true);
+                editor.putString(LONGTITUDE, lon);
+                editor.putString(LATITUDE, lat);
+                editor.commit();
+
+//                prefs.edit().putString("Lat",String.valueOf(latLng.latitude)).commit();
+//                prefs.edit().putString("Lng",String.valueOf(latLng.longitude)).commit();
+
+//                    mMap.clear();
+//                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+//                    mMap.addMarker(markerOptions);
             }
         });
 
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        Boolean checkedout = sharedPreferences.getBoolean(LOGGEDIN_SHARED_PREF, false);
 
+        if (checkedout) {
+            for(int i = 0; i < markerCount; i++) {
+                SharedPreferences sharedPreferencess = getSharedPreferences(MapsActivity.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                String lon = sharedPreferencess.getString(MapsActivity.LONGTITUDE, "Not Available");
+                String lat = sharedPreferencess.getString(MapsActivity.LATITUDE, "Not Available");
+
+                double lo = Double.parseDouble(lon);
+                double la = Double.parseDouble(lat);
+
+                LatLng resumedPosition = new LatLng(lo, la);
+
+//
+//            mMap.addMarker(new MarkerOptions().position(resumedPosition).title("Marker"));
+//            mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(resumedPosition));
+
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(resumedPosition).draggable(false));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(resumedPosition));
+            }
+
+
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
